cdf7214e4720b62a74972c10fb002a675e663bf0
ghumman3131 <charanghumman@gmail.com>
2017-04-05 08:47:50 +0530

    first commit

---
+++ app/src/main/java/tech/inception/location/MainActivity.java
@@ -0,0 +1,42 @@
+package tech.inception.location;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        LocationManager mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location l = mLocationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+
+        Double lat = l.getLatitude();
+
+        Double lng = l.getLongitude();
+
+        Toast.makeText(MainActivity.this, String.valueOf(lat) + "  and " + String.valueOf(lng), Toast.LENGTH_SHORT).show();
+
+
+    }
+}
